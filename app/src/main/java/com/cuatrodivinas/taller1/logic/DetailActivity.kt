468485e67126cf405aa82db30de53df29f5e2050
alexoberco <alejandro.bermudez.fajardo@gmail.com>
2024-08-26 19:08:40 -0500

    feat: additional features added

--- app/src/main/java/com/cuatrodivinas/taller1/logic/DetailActivity.kt
+++ app/src/main/java/com/cuatrodivinas/taller1/logic/DetailActivity.kt
@@ -1,21 +1,47 @@
 package com.cuatrodivinas.taller1.logic
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.ImageView
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.cuatrodivinas.taller1.R
+import org.json.JSONObject
 
 class DetailActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val destinoJson = intent.getStringExtra("destino")
+        if (destinoJson != null) {
+            val destino = JSONObject(destinoJson)
+
+            val cityTextView: TextView = findViewById(R.id.city)
+            val countryTextView: TextView = findViewById(R.id.country)
+            val categoryTextView: TextView = findViewById(R.id.category)
+            val planTextView: TextView = findViewById(R.id.plan)
+            val priceTextView: TextView = findViewById(R.id.price)
+            val buttonFavorites: Button = findViewById(R.id.button_favorites)
+
+            cityTextView.text = destino.getString("nombre")
+            countryTextView.text = destino.getString("pais")
+            categoryTextView.text = destino.getString("categoria")
+            planTextView.text = destino.getString("plan")
+            priceTextView.text = destino.getInt("precio").toString()
+
+            val showButton = intent.getBooleanExtra("showButton", false)
+            buttonFavorites.visibility = if (showButton) View.VISIBLE else View.GONE
+        } else {
+            finish() // Close the activity or show an error message
+        }
+
+        val arrowBack: ImageView = findViewById(R.id.arrow_back)
+        arrowBack.setOnClickListener {
+            finish() // Finish the current activity and return to the previous one
         }
     }
 }
--- app/src/main/java/com/cuatrodivinas/taller1/logic/ExplorarDestinos.kt
+++ app/src/main/java/com/cuatrodivinas/taller1/logic/ExplorarDestinos.kt
@@ -2,81 +2,66 @@ package com.cuatrodivinas.taller1.logic
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.cuatrodivinas.taller1.MainActivity
 import com.cuatrodivinas.taller1.R
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
 import java.io.InputStream
-import java.io.Serializable
 
 class ExplorarDestinos : AppCompatActivity() {
-    private lateinit var detalles:ListView
-    private lateinit var destinos:JSONArray
-    private lateinit var destinosNombre:MutableList<String>
-    private lateinit var categoria:String
+    private lateinit var detalles: ListView
+    private lateinit var destinos: JSONArray
+    private lateinit var destinosNombre: MutableList<String>
+    private lateinit var categoria: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explorar_destinos)
         destinosNombre = mutableListOf()
-        categoria = intent.getStringExtra("categoria")!!
+        categoria = intent.getStringExtra("categoria") ?: "todos"
         inicializarElementos()
         eventoDetalles()
     }
 
-    fun inicializarElementos(){
+    private fun inicializarElementos() {
         detalles = findViewById(R.id.destinosListView)
-        //ListView
-        val json = JSONObject(cargarJson())
+        val json = JSONObject(cargarJson() ?: return)
         destinos = json.getJSONArray("destinos")
-        for (i in 0 until destinos.length()){
+        for (i in 0 until destinos.length()) {
             val jsonObject = destinos.getJSONObject(i)
-            if(categoria.equals("todos", true) ||
-                categoria.equals(jsonObject.getString("categoria"), true)){
+            if (categoria.equals("todos", true) || categoria.equals(jsonObject.getString("categoria"), true)) {
                 destinosNombre.add(jsonObject.getString("nombre"))
             }
         }
 
-        val adaptador = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            destinosNombre)
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1, destinosNombre)
         detalles.adapter = adaptador
     }
 
-    fun cargarJson(): String?{
-        var json: String? = null
-        try {
+    private fun cargarJson(): String? {
+        return try {
             val isStream: InputStream = assets.open("destinos.json")
-            val size:Int = isStream.available()
+            val size: Int = isStream.available()
             val buffer = ByteArray(size)
             isStream.read(buffer)
             isStream.close()
-            json = String(buffer, Charsets.UTF_8)
+            String(buffer, Charsets.UTF_8)
         } catch (ex: IOException) {
             ex.printStackTrace()
-            return null
+            null
         }
-        return json
     }
 
-    fun eventoDetalles(){
-        detalles.setOnItemClickListener(object : AdapterView.OnItemClickListener {
-            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val intent = Intent(this@ExplorarDestinos, MainActivity::class.java)
-                val bundle = Bundle()
-                bundle.putString("destinos", destinos.get(position).toString())
-                intent.putExtra("bundle", bundle)
-                startActivity(intent)
-            }
-        })
+    private fun eventoDetalles() {
+        detalles.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this@ExplorarDestinos, DetailActivity::class.java)
+            val destino = destinos.getJSONObject(position).toString()
+            intent.putExtra("destino", destino)
+            intent.putExtra("showButton", true)
+            startActivity(intent)
+        }
     }
 }
